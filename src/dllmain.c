9dce73b4e674a7a6a72b9998d8fec7bb340912b9
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2022-03-10 19:53:26 +1300

    Use calloc instead of fixed size array

--- src/dllmain.c
+++ src/dllmain.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <windows.h>
 
-#define ARRAY_SIZE 255
+int num = 0;
 
-wchar_t olds[ARRAY_SIZE][ARRAY_SIZE];
-wchar_t news[ARRAY_SIZE][ARRAY_SIZE];
+wchar_t **olds;
+wchar_t **news;
 
 HOOK (void, __stdcall, DivaDrawTextW, 0x140198380, void *param, uint32_t flags,
 	  const wchar_t **text)
 {
-	for (int i = 0; i < ARRAY_SIZE; i++)
+	for (int i = 0; i < num; i++)
 		{
 			if (wcscmp (*text, olds[i]) == 0)
 				{
@@ -66,16 +66,67 @@ DllMain (HMODULE mod, DWORD cause, void *ctx)
 					toml_datum_t new = toml_string_in (translation, "new");
 					if (!old.ok || !new.ok)
 						continue;
-
-					MultiByteToWideChar (CP_UTF8, 0, old.u.s, -1, olds[i],
-										 ARRAY_SIZE);
-					MultiByteToWideChar (CP_UTF8, 0, new.u.s, -1, news[i],
-										 ARRAY_SIZE);
+					num++;
 
 					free (old.u.s);
 					free (new.u.s);
 				}
+			toml_free (translationConfig);
+		}
+	while (FindNextFileA (file, &fd));
 
+	olds = calloc (num, sizeof (wchar_t *));
+	news = calloc (num, sizeof (wchar_t *));
+	num = 0;
+
+	file = FindFirstFileA (configPath ("translations\\*.toml"), &fd);
+	do
+		{
+			if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+				continue;
+
+			char filepath[MAX_PATH];
+			strcpy (filepath, configPath ("translations\\"));
+			strcat (filepath, fd.cFileName);
+			toml_table_t *translationConfig = openConfig (filepath);
+
+			if (!readConfigBool (translationConfig, "enabled", false))
+				{
+					toml_free (translationConfig);
+					continue;
+				}
+
+			toml_array_t *translationArray
+				= toml_array_in (translationConfig, "translation");
+			for (int i = 0;; i++)
+				{
+					toml_table_t *translation
+						= toml_table_at (translationArray, i);
+					if (!translation)
+						break;
+					toml_datum_t old = toml_string_in (translation, "old");
+					toml_datum_t new = toml_string_in (translation, "new");
+					if (!old.ok || !new.ok)
+						continue;
+
+					int sizeOld
+						= MultiByteToWideChar (CP_UTF8, 0, old.u.s, -1, 0, 0);
+					int sizeNew
+						= MultiByteToWideChar (CP_UTF8, 0, new.u.s, -1, 0, 0);
+
+					olds[num] = calloc (sizeOld, sizeof (wchar_t));
+					news[num] = calloc (sizeNew, sizeof (wchar_t));
+
+					MultiByteToWideChar (CP_UTF8, 0, old.u.s, -1, olds[num],
+										 sizeOld);
+					MultiByteToWideChar (CP_UTF8, 0, new.u.s, -1, news[num],
+										 sizeNew);
+
+					num++;
+
+					free (old.u.s);
+					free (new.u.s);
+				}
 			toml_free (translationConfig);
 		}
 	while (FindNextFileA (file, &fd));
