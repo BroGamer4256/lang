b145f828c637df37f5c082e3f572f61b3742660a
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2022-03-15 14:30:03 +1300

    Simplify code

--- src/dllmain.c
+++ src/dllmain.c
@@ -2,130 +2,14 @@
 #include <stdio.h>
 #include <windows.h>
 
-#define CURRENT_GAME_SUB_STATE_ADDRESS 0x140EDA82C
-
-enum SubGameState : uint32_t {
-	SUB_DATA_INITIALIZE,
-	SUB_SYSTEM_STARTUP,
-	SUB_SYSTEM_STARTUP_ERROR,
-	SUB_WARNING,
-	SUB_LOGO,
-	SUB_RATING,
-	SUB_DEMO,
-	SUB_TITLE,
-	SUB_RANKING,
-	SUB_SCORE_RANKING,
-	SUB_CM,
-	SUB_PHOTO_MODE_DEMO,
-	SUB_SELECTOR,
-	SUB_GAME_MAIN,
-	SUB_GAME_SEL,
-	SUB_STAGE_RESULT,
-	SUB_SCREEN_SHOT_SEL,
-	SUB_SCREEN_SHOT_RESULT,
-	SUB_GAME_OVER,
-	SUB_DATA_TEST_MAIN,
-	SUB_DATA_TEST_MISC,
-	SUB_DATA_TEST_OBJ,
-	SUB_DATA_TEST_STG,
-	SUB_DATA_TEST_MOT,
-	SUB_DATA_TEST_COLLISION,
-	SUB_DATA_TEST_SPR,
-	SUB_DATA_TEST_AET,
-	SUB_DATA_TEST_AUTH_3D,
-	SUB_DATA_TEST_CHR,
-	SUB_DATA_TEST_ITEM,
-	SUB_DATA_TEST_PERF,
-	SUB_DATA_TEST_PVSCRIPT,
-	SUB_DATA_TEST_PRINT,
-	SUB_DATA_TEST_CARD,
-	SUB_DATA_TEST_OPD,
-	SUB_DATA_TEST_SLIDER,
-	SUB_DATA_TEST_GLITTER,
-	SUB_DATA_TEST_GRAPHICS,
-	SUB_DATA_TEST_COLLECTION_CARD,
-	SUB_TEST_MODE_MAIN,
-	SUB_APP_ERROR,
-	SUB_MAX,
-};
-
-struct {
-	const char *string;
-	enum SubGameState state;
-} ConfigSubGameStates[] = {
-	{ "SUB_DATA_INITIALIZE", SUB_DATA_INITIALIZE },
-	{ "SUB_SYSTEM_STARTUP", SUB_SYSTEM_STARTUP },
-	{ "SUB_SYSTEM_STARTUP_ERROR", SUB_SYSTEM_STARTUP_ERROR },
-	{ "SUB_WARNING", SUB_WARNING },
-	{ "SUB_LOGO", SUB_LOGO },
-	{ "SUB_RATING", SUB_RATING },
-	{ "SUB_DEMO", SUB_DEMO },
-	{ "SUB_TITLE", SUB_TITLE },
-	{ "SUB_RANKING", SUB_RANKING },
-	{ "SUB_SCORE_RANKING", SUB_SCORE_RANKING },
-	{ "SUB_CM", SUB_CM },
-	{ "SUB_PHOTO_MODE_DEMO", SUB_PHOTO_MODE_DEMO },
-	{ "SUB_SELECTOR", SUB_SELECTOR },
-	{ "SUB_GAME_MAIN", SUB_GAME_MAIN },
-	{ "SUB_GAME_SEL", SUB_GAME_SEL },
-	{ "SUB_STAGE_RESULT", SUB_STAGE_RESULT },
-	{ "SUB_SCREEN_SHOT_SEL", SUB_SCREEN_SHOT_SEL },
-	{ "SUB_SCREEN_SHOT_RESULT", SUB_SCREEN_SHOT_RESULT },
-	{ "SUB_GAME_OVER", SUB_GAME_OVER },
-	{ "SUB_DATA_TEST_MAIN", SUB_DATA_TEST_MAIN },
-	{ "SUB_DATA_TEST_MISC", SUB_DATA_TEST_MISC },
-	{ "SUB_DATA_TEST_OBJ", SUB_DATA_TEST_OBJ },
-	{ "SUB_DATA_TEST_STG", SUB_DATA_TEST_STG },
-	{ "SUB_DATA_TEST_MOT", SUB_DATA_TEST_MOT },
-	{ "SUB_DATA_TEST_COLLISION", SUB_DATA_TEST_COLLISION },
-	{ "SUB_DATA_TEST_SPR", SUB_DATA_TEST_SPR },
-	{ "SUB_DATA_TEST_AET", SUB_DATA_TEST_AET },
-	{ "SUB_DATA_TEST_AUTH_3D", SUB_DATA_TEST_AUTH_3D },
-	{ "SUB_DATA_TEST_CHR", SUB_DATA_TEST_CHR },
-	{ "SUB_DATA_TEST_ITEM", SUB_DATA_TEST_ITEM },
-	{ "SUB_DATA_TEST_PERF", SUB_DATA_TEST_PERF },
-	{ "SUB_DATA_TEST_PVSCRIPT", SUB_DATA_TEST_PVSCRIPT },
-	{ "SUB_DATA_TEST_PRINT", SUB_DATA_TEST_PRINT },
-	{ "SUB_DATA_TEST_CARD", SUB_DATA_TEST_CARD },
-	{ "SUB_DATA_TEST_OPD", SUB_DATA_TEST_OPD },
-	{ "SUB_DATA_TEST_SLIDER", SUB_DATA_TEST_SLIDER },
-	{ "SUB_DATA_TEST_GLITTER", SUB_DATA_TEST_GLITTER },
-	{ "SUB_DATA_TEST_GRAPHICS", SUB_DATA_TEST_GRAPHICS },
-	{ "SUB_DATA_TEST_COLLECTION_CARD", SUB_DATA_TEST_COLLECTION_CARD },
-	{ "SUB_TEST_MODE_MAIN", SUB_TEST_MODE_MAIN },
-	{ "SUB_APP_ERROR", SUB_APP_ERROR },
-};
-
-enum SubGameState
-strToSubGameState (const char *str) {
-	for (int i = 0; i < COUNTOFARR (ConfigSubGameStates); i++)
-		if (strcmp (str, ConfigSubGameStates[i].string) == 0)
-			return ConfigSubGameStates[i].state;
-	return SUB_MAX;
-}
-
 int num = 0;
 
 wchar_t **olds;
 wchar_t **news;
-enum SubGameState *states;
 
 HOOK (void, __stdcall, DivaDrawTextW, 0x140198380, void *param, uint32_t flags,
 	  const wchar_t **text) {
 	for (int i = 0; i < num; i++) {
-		if (states[i] != SUB_MAX) {
-			if (states[i] == SUB_GAME_SEL || states[i] == SUB_SELECTOR) {
-				if (*(enum SubGameState *)CURRENT_GAME_SUB_STATE_ADDRESS
-						!= SUB_GAME_SEL
-					&& *(enum SubGameState *)CURRENT_GAME_SUB_STATE_ADDRESS
-						   != SUB_SELECTOR)
-					continue;
-			} else if (*(enum SubGameState *)CURRENT_GAME_SUB_STATE_ADDRESS
-					   != states[i]) {
-				continue;
-			}
-		}
-
 		if (wcscmp (*text, olds[i]) == 0) {
 			const wchar_t *ptrs[2];
 			ptrs[0] = news[i];
@@ -198,7 +82,6 @@ DllMain (HMODULE mod, DWORD cause, void *ctx) {
 
 	olds = calloc (num, sizeof (wchar_t *));
 	news = calloc (num, sizeof (wchar_t *));
-	states = calloc (num, sizeof (enum SubGameState));
 	num = 0;
 
 	file = FindFirstFileA (configPath ("translations\\*.toml"), &fd);
@@ -210,6 +93,9 @@ DllMain (HMODULE mod, DWORD cause, void *ctx) {
 		strcpy (filepath, configPath ("translations\\"));
 		strcat (filepath, fd.cFileName);
 		toml_table_t *translationConfig = openConfig (filepath);
+
+		if (!translationConfig)
+			continue;
 
 		if (!readConfigBool (translationConfig, "enabled", false)) {
 			toml_free (translationConfig);
@@ -224,7 +110,6 @@ DllMain (HMODULE mod, DWORD cause, void *ctx) {
 				break;
 			toml_datum_t old = toml_string_in (translation, "old");
 			toml_datum_t new = toml_string_in (translation, "new");
-			toml_datum_t state = toml_string_in (translation, "state");
 			if (!old.ok || !new.ok)
 				continue;
 
@@ -237,17 +122,10 @@ DllMain (HMODULE mod, DWORD cause, void *ctx) {
 			MultiByteToWideChar (CP_UTF8, 0, old.u.s, -1, olds[num], sizeOld);
 			MultiByteToWideChar (CP_UTF8, 0, new.u.s, -1, news[num], sizeNew);
 
-			if (state.ok)
-				states[num] = strToSubGameState (state.u.s);
-			else
-				states[num] = SUB_MAX;
-
 			num++;
 
 			free (old.u.s);
 			free (new.u.s);
-			if (state.ok)
-				free (state.u.s);
 		}
 		toml_free (translationConfig);
 	} while (FindNextFileA (file, &fd));
