5e7d74bfc37223d3705ca621070ac3b3bb6079ec
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2022-05-01 21:38:56 +1200

    owo

--- src/dllmain.c
+++ src/dllmain.c
@@ -1,11 +1,21 @@
 #include "helpers.h"
 #include <stdio.h>
+#include <time.h>
 #include <windows.h>
+
+void owoDrawTextW (void *param, uint32_t flags, const wchar_t **text);
+
+struct linked_list {
+	const wchar_t *original;
+	wchar_t *owoified;
+	struct linked_list *next;
+};
 
 int num = 0;
 
 wchar_t **olds;
 wchar_t **news;
+struct linked_list *translated;
 
 HOOK (void, __stdcall, DivaDrawTextW, 0x140198380, void *param, uint32_t flags,
 	  const wchar_t **text) {
@@ -14,7 +24,7 @@ HOOK (void, __stdcall, DivaDrawTextW, 0x140198380, void *param, uint32_t flags,
 			const wchar_t *ptrs[2];
 			ptrs[0] = news[i];
 			ptrs[1] = (wchar_t *)((uint64_t)ptrs[0] + wcslen (news[i]) * 2);
-			originalDivaDrawTextW (param, flags, &ptrs[0]);
+			owoDrawTextW (param, flags, &ptrs[0]);
 			return;
 		}
 	}
@@ -26,6 +36,118 @@ HOOK (void, __stdcall, DivaDrawTextW, 0x140198380, void *param, uint32_t flags,
 	HANDLE consoleHandle = GetStdHandle (STD_OUTPUT_HANDLE);
 	WriteConsoleW (consoleHandle, buf, 256, NULL, NULL);
 	*/
+}
+wchar_t *faces[] = { L"owo", L"UwU", L">w<", L"^w^" };
+
+wchar_t *
+owoTranslate (const wchar_t *text) {
+	int len = 0;
+	for (int i = 0; i < wcslen (text); i++) {
+		len++;
+		switch (text[i]) {
+		case L'!':
+			len += 4;
+			break;
+		case L'n':
+		case L'N':
+			if (text[i + 1] == L'a' || text[i + 1] == L'e'
+				|| text[i + 1] == L'i' || text[i + 1] == L'o'
+				|| text[i + 1] == L'u' || text[i + 1] == L'A'
+				|| text[i + 1] == L'E' || text[i + 1] == L'I'
+				|| text[i + 1] == L'O' || text[i + 1] == L'U')
+				len++;
+			break;
+		default:
+			break;
+		}
+	}
+	srand (time (0));
+	wchar_t *owoified = calloc (len + 1, sizeof (wchar_t));
+	int owoI = 0;
+	for (int i = 0; i < wcslen (text); i++) {
+		switch (text[i]) {
+		case L'!':;
+			wchar_t *face = faces[rand () % COUNTOFARR (faces)];
+			owoified[owoI] = L' ';
+			owoified[owoI + 1] = face[0];
+			owoified[owoI + 2] = face[1];
+			owoified[owoI + 3] = face[2];
+			owoified[owoI + 4] = L' ';
+			owoI += 4;
+			break;
+		case L'n':
+		case L'N':
+			if (text[i + 1] == L'a' || text[i + 1] == L'e'
+				|| text[i + 1] == L'i' || text[i + 1] == L'o'
+				|| text[i + 1] == L'u' || text[i + 1] == L'A'
+				|| text[i + 1] == L'E' || text[i + 1] == L'I'
+				|| text[i + 1] == L'O' || text[i + 1] == L'U') {
+				owoified[owoI] = text[i];
+				owoified[owoI + 1] = L'y';
+				owoI++;
+			} else {
+				owoified[owoI] = text[i];
+			}
+			break;
+		default:
+			owoified[owoI] = text[i];
+			break;
+		}
+		owoI++;
+	}
+	owoified[owoI] = 0;
+
+	return owoified;
+}
+
+struct linked_list *
+lookup (struct linked_list *list, const wchar_t *text) {
+	if (list == 0 || list->next == 0) {
+		return 0;
+	}
+	if (list->original == 0 || wcscmp (list->original, text) != 0) {
+		return lookup (list->next, text);
+	}
+	return list;
+}
+
+struct linked_list *
+last (struct linked_list *list) {
+	if (list == 0) {
+		return 0;
+	}
+	struct linked_list *last = list;
+	while (last->next != 0) {
+		last = last->next;
+	}
+	return last;
+}
+
+void
+add (struct linked_list *list, struct linked_list *new) {
+	struct linked_list *last_list = last (list);
+	if (last_list == 0) {
+		list = new;
+		return;
+	}
+	last_list->next = new;
+}
+
+void
+owoDrawTextW (void *param, uint32_t flags, const wchar_t **text) {
+	struct linked_list *owoified = lookup (translated, *text);
+	if (owoified == 0) {
+		wchar_t *newText = owoTranslate (*text);
+		owoified = malloc (sizeof (struct linked_list));
+		owoified->original = *text;
+		owoified->owoified = newText;
+		add (translated, owoified);
+	}
+	const wchar_t *ptrs[2];
+	ptrs[0] = owoified->owoified;
+	ptrs[1] = (wchar_t *)((uint64_t)ptrs[0] + wcslen (owoified->owoified) * 2);
+
+	originalDivaDrawTextW (param, flags, &ptrs[0]);
 }
 
 BOOL WINAPI
